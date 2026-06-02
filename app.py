728312742a4f65fbe01be68f465ff37cd3808f12
luiszagastizabal15-tech <luis.zagastizabal.15@gmail.com>
2026-06-01 20:19:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+# Ingresar titulo
 st.title("Proyecto Final Diploma BI")
-
+# Ingresar titulo como barra lateral
 st.sidebar.title("Parámetros")
+# Ingresar imagenes
+st.image("Pythonlogo.png")
+st.sidebar.image("DMCLogo.png")
