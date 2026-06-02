4c6c75d96d3b6cacf1e532c7b07e0b50d34f8ad8
luiszagastizabal15-tech <luis.zagastizabal.15@gmail.com>
2026-06-01 20:31:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ st.title("Proyecto Final Diploma BI")
 # Ingresar titulo como barra lateral
 st.sidebar.title("Parámetros")
 # Ingresar imagenes
-st.image("Pythonlogo.png", width = 300)
-st.sidebar.image("DMCLogo.png", width = 150)
+st.image("Pythonlogo.png", width = 200)
+st.sidebar.image("DMCLogo.png", width = 100)
 
 
 st.write("Elaborado por: Gustavo Rodriguez")
