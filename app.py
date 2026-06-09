001722b807c83fd6e936ff2ee170c4749783acaa
luiszagastizabal15-tech <luis.zagastizabal.15@gmail.com>
2026-06-08 19:53:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,36 +21,39 @@ st.write("Elaborado por: Gustavo Rodriguez")
 
 modulos = st.sidebar.selectbox('Seleccione un módulo',["Home", "Carga y perfil del dataset", "Procesamiento de datos", "Analisis visual"])
 
+if modulos == "Home":
+    st.write("Bienvenido a la aplicacion")
 
-# Creamos un cargador de archivos para subir archivos Excel o CSV
-archivo = st.file_uploader("Cargue el archivo excel o csv")
-
-# Validamos si el usuario cargó un archivo
-if archivo is not None:
-
-    # Validamos si el archivo cargado tiene extensión .csv
-    if archivo.name.endswith(".csv"):
-
-        # Leemos el archivo CSV y lo guardamos en un DataFrame
-        data = pd.read_csv(archivo)
-
-        # Mostramos el DataFrame en la aplicación
-        st.write(data)
-
-    # Validamos si el archivo cargado tiene extensión .xlsx
-    elif archivo.name.endswith(".xlsx"):
-
-        # Leemos el archivo Excel y lo guardamos en un DataFrame
-        data = pd.read_excel(archivo)
-
-        # Mostramos el DataFrame en la aplicación
-        st.write(data)
-
-    # Si el archivo no es CSV ni Excel, mostramos un mensaje de error
+elif modulos == "Carga y perfil del dataset":
+    # Creamos un cargador de archivos para subir archivos Excel o CSV
+    archivo = st.file_uploader("Cargue el archivo excel o csv")
+    
+    # Validamos si el usuario cargó un archivo
+    if archivo is not None:
+    
+        # Validamos si el archivo cargado tiene extensión .csv
+        if archivo.name.endswith(".csv"):
+    
+            # Leemos el archivo CSV y lo guardamos en un DataFrame
+            data = pd.read_csv(archivo)
+    
+            # Mostramos el DataFrame en la aplicación
+            st.write(data)
+    
+        # Validamos si el archivo cargado tiene extensión .xlsx
+        elif archivo.name.endswith(".xlsx"):
+    
+            # Leemos el archivo Excel y lo guardamos en un DataFrame
+            data = pd.read_excel(archivo)
+    
+            # Mostramos el DataFrame en la aplicación
+            st.write(data)
+    
+        # Si el archivo no es CSV ni Excel, mostramos un mensaje de error
+        else:
+            st.write("Formato no válido")
+    
+    # Si el usuario no ha cargado ningún archivo, mostramos un mensaje
     else:
-        st.write("Formato no válido")
-
-# Si el usuario no ha cargado ningún archivo, mostramos un mensaje
-else:
-    st.write("Por favor cargue su archivo")
+        st.write("Por favor cargue su archivo")
     
